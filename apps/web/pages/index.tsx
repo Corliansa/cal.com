890e47ebbf386c8157ae121dff84595bb90c1957
Corliansa Kusumah <corliansa@gmail.com>
2023-04-11 00:46:49 +0200

    changed index and added privacy and terms

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,19 +1,75 @@
-import type { GetServerSidePropsContext } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
+import type { Session } from "next-auth";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
+import { APP_NAME, COMPANY_NAME, LOGO } from "@calcom/lib/constants";
 
-function RedirectPage() {
-  return;
-}
+export const Index: NextPage<{ session: Session | null }> = ({ session }) => {
+  const { push } = useRouter();
+  return (
+    <div className="isolate min-h-screen bg-white text-gray-700">
+      <div className="px-6 pt-6 lg:px-8">
+        <div>
+          <nav className="flex h-9 items-center justify-center" aria-label="Global">
+            <div className="flex justify-center" aria-label="Global">
+              <Link href="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">{APP_NAME}</span>
+                <img className="mb-auto h-4" src={LOGO} alt={`${APP_NAME} Logo`} />
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <main>
+        <div className="relative px-6 lg:px-8">
+          <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+            <h1 className="text-5xl font-semibold tracking-tight sm:text-center sm:text-7xl">Calendso</h1>
+            <p className="my-6 text-lg leading-8 text-gray-600 sm:text-center">
+              The open source Calendly alternative. You are in charge of your own data, workflow and
+              appearance.
+            </p>
+            <div className="flex items-center justify-center gap-4">
+              <button
+                onClick={() => {
+                  if (!session?.user?.id) {
+                    void push("/auth/login");
+                  } else {
+                    void push("/event-types");
+                  }
+                }}
+                className="rounded-md bg-gray-700 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+                Get started
+              </button>
+              <Link href="/auth/login" className="text-base font-semibold leading-7 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </Link>
+            </div>
+            <hr className="mt-6 mb-3 border-gray-300" />
+            <div className="flex items-center justify-between text-sm">
+              <div>
+                &copy; {new Date().getFullYear()}{" "}
+                <Link href="https://go.cal.com/credits" target="_blank" className="hover:underline">
+                  {COMPANY_NAME}
+                </Link>
+              </div>
+              <div className="flex gap-2">
+                <Link href="/privacy">Privacy Policy</Link>
+                <Link href="/terms">Terms of Service</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export async function getServerSideProps({ req, res }: GetServerSidePropsContext) {
   const session = await getServerSession({ req, res });
 
-  if (!session?.user?.id) {
-    return { redirect: { permanent: false, destination: "/auth/login" } };
-  }
-
-  return { redirect: { permanent: false, destination: "/event-types" } };
+  return { props: { session } };
 }
 
-export default RedirectPage;
+export default Index;
---
+++ apps/web/pages/privacy.tsx
@@ -0,0 +1,89 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+import { APP_NAME, LOGO, SUPPORT_MAIL_ADDRESS } from "@calcom/lib/constants";
+
+export const Privacy: NextPage = () => {
+  return (
+    <div className="isolate min-h-screen bg-white text-gray-700">
+      <div className="px-6 pt-6 lg:px-8">
+        <div>
+          <nav className="flex h-9 items-center justify-center" aria-label="Global">
+            <div className="flex justify-center" aria-label="Global">
+              <Link href="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">{APP_NAME}</span>
+                <img className="mb-auto h-4" src={LOGO} alt={`${APP_NAME} Logo`} />
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <main>
+        <div className="relative px-6 lg:px-8">
+          <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+            <h1 className="mb-2 text-4xl font-bold">Privacy & Policy</h1>
+            <div className="mt-4 flex flex-col gap-4">
+              <p>
+                {APP_NAME} is committed to protecting the privacy of our users in compliance with the General
+                Data Protection Regulation (GDPR). This Privacy Policy describes how we collect, use, and
+                share your personal information.
+              </p>
+
+              <p>
+                <strong>Information We Collect:</strong> We collect personal information only with your
+                explicit consent and when it is necessary to provide our services, such as your name, email
+                address, and content that you create using the Service.
+              </p>
+              <p>
+                <strong>Use of Information:</strong> We use your personal information to provide our services,
+                to communicate with you, and to improve our website and services. You have the right to
+                access, correct, or delete your personal information at any time, and we will make reasonable
+                efforts to accommodate your requests.
+              </p>
+              <p>
+                <strong>Cookies:</strong> {APP_NAME} uses cookies to enhance your experience on our website.
+                Cookies are small files that are stored on your device and help us to remember your
+                preferences and improve our website. You can choose to disable cookies in your browser
+                settings.
+              </p>
+              <p>
+                <strong>Security:</strong> {APP_NAME} takes reasonable measures to protect your personal
+                information from unauthorized access, use, or disclosure. We use encryption, access controls,
+                and other security measures to safeguard your data. However, we cannot guarantee the security
+                of your personal information.
+              </p>
+              <p>
+                <strong>Data Retention:</strong> We retain your personal information only for as long as
+                necessary to provide our services or as required by law. You have the right to request that we
+                delete your personal information, and we will make reasonable efforts to accommodate your
+                request.
+              </p>
+              <p>
+                <strong>Children&apos;s Privacy:</strong> {APP_NAME} is not intended for use by children under
+                the age of 16. We do not knowingly collect personal information from children under the age of
+                16.
+              </p>
+              <p>
+                <strong>Third-Party Services:</strong> {APP_NAME} may use third-party services, such as
+                analytics tools, to help us improve our website and services. These third-party services may
+                collect and use your personal information in accordance with their own privacy policies. We
+                will only work with third-party services that comply with GDPR.
+              </p>
+              <p>
+                <strong>Changes to Privacy Policy:</strong> {APP_NAME} may update this Privacy Policy from
+                time to time. We will notify you of any material changes to this Privacy Policy, and we
+                encourage you to review this Privacy Policy periodically.
+              </p>
+              <p>
+                If you have any questions or concerns about our Privacy Policy or how we handle your personal
+                information, please don&apos;t hesitate to contact us at {SUPPORT_MAIL_ADDRESS}
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Privacy;
