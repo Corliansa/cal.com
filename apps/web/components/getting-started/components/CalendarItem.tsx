91a1b2e8ba956af523ffd73f87e961a65b1518dd
Corliansa Kusumah <corliansa@gmail.com>
2023-04-30 10:42:00 +0200

    fix add calendar on getting started

--- apps/web/components/getting-started/components/CalendarItem.tsx
+++ apps/web/components/getting-started/components/CalendarItem.tsx
@@ -1,7 +1,8 @@
+import useAddAppMutation from "@calcom/app-store/_utils/useAddAppMutation";
 import { InstallAppButtonWithoutPlanCheck } from "@calcom/app-store/components";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import type { App } from "@calcom/types/App";
-import { Button } from "@calcom/ui";
+import { Button, showToast } from "@calcom/ui";
 
 interface ICalendarItem {
   title: string;
@@ -13,6 +14,17 @@ interface ICalendarItem {
 const CalendarItem = (props: ICalendarItem) => {
   const { title, logo, type } = props;
   const { t } = useLocale();
+
+  const mutation = useAddAppMutation(null, {
+    onSuccess: (data) => {
+      if (data?.setupPending) return;
+      showToast(t("app_successfully_installed"), "success");
+    },
+    onError: (error) => {
+      if (error instanceof Error) showToast(error.message || t("app_could_not_be_installed"), "error");
+    },
+  });
+
   return (
     <div className="flex flex-row items-center justify-between p-5">
       <div className="flex items-center space-x-3">
@@ -21,19 +33,30 @@ const CalendarItem = (props: ICalendarItem) => {
       </div>
       <InstallAppButtonWithoutPlanCheck
         type={type}
-        render={(buttonProps) => (
-          <Button
-            {...buttonProps}
-            color="secondary"
-            type="button"
-            onClick={(event) => {
-              // Save cookie key to return url step
-              document.cookie = `return-to=${window.location.href};path=/;max-age=3600;SameSite=Lax`;
-              buttonProps && buttonProps.onClick && buttonProps?.onClick(event);
-            }}>
-            {t("connect")}
-          </Button>
-        )}
+        render={({ useDefaultComponent, ...props }) => {
+          if (useDefaultComponent) {
+            props = {
+              ...props,
+              onClick: () => {
+                mutation.mutate({ type });
+              },
+              loading: mutation.isLoading,
+            };
+          }
+          return (
+            <Button
+              {...props}
+              color="secondary"
+              type="button"
+              onClick={(event) => {
+                // Save cookie key to return url step
+                document.cookie = `return-to=${window.location.href};path=/;max-age=3600;SameSite=Lax`;
+                props && props.onClick && props?.onClick(event);
+              }}>
+              {t("connect")}
+            </Button>
+          );
+        }}
       />
     </div>
   );
